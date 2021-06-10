7c4918998a085fb7693154d61d7cf21df8a05db6
Niket <58136275+bunnyscac@users.noreply.github.com>
2021-06-10 18:32:46 +0530

    Update face.py

--- Face-Cogs/face.py
+++ Face-Cogs/face.py
@@ -1,6 +1,6 @@
 import cv2
 
-print("hello")
+print("hello there human, your webcam will open now and you'll see the magic for youself")
 
 trained_face_data = cv2.CascadeClassifier(
     'haarcascade_frontalface_default.xml')
